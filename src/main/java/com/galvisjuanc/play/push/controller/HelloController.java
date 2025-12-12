0ee56d5accd8a5c4763409ef43faf2b782df4cef
Juan Galvis <Galvis>
2025-12-12 09:25:06 -0500

    Small change in the HelloController

--- src/main/java/com/galvisjuanc/play/push/controller/HelloController.java
+++ src/main/java/com/galvisjuanc/play/push/controller/HelloController.java
@@ -11,13 +11,13 @@ public class HelloController {
     private final String platform;
     private final PlayPushAiService playPushAiService;
 
-    public HelloController(@Value("spring.application.name") String platform, PlayPushAiService playPushAiService) {
+    public HelloController(@Value("${spring.application.name}") String platform, PlayPushAiService playPushAiService) {
         this.platform = platform;
         this.playPushAiService = playPushAiService;
     }
 
     @GetMapping("/")
     public String hello() {
-        return this.playPushAiService.generateGreeting();
+        return this.playPushAiService.generateGreeting(platform);
     }
 }
