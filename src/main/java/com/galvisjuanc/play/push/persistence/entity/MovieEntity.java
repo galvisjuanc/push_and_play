2a11f05e83b72c77a4966186788302fc74d32e31
Juan Galvis <Galvis>
2025-12-19 07:39:08 -0500

    other properties included and created getters/setters in MovieEntity

--- src/main/java/com/galvisjuanc/play/push/persistence/entity/MovieEntity.java
+++ src/main/java/com/galvisjuanc/play/push/persistence/entity/MovieEntity.java
@@ -1,5 +1,6 @@
 package com.galvisjuanc.play.push.persistence.entity;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class MovieEntity {
@@ -7,4 +8,54 @@ public class MovieEntity {
     private Integer duration;
     private String genre;
     private LocalDate releaseDate;
+    private BigDecimal clasificacion;
+    private String state;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public BigDecimal getClasificacion() {
+        return clasificacion;
+    }
+
+    public void setClasificacion(BigDecimal clasificacion) {
+        this.clasificacion = clasificacion;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
 }
