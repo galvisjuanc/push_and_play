e887ab8b881a5a07d2b3fd056e9be9f6d9a3f96a
Juan Galvis <Galvis>
2026-01-04 10:54:29 -0500

    Add the RequestMapping to generalize the name of the main api

--- src/main/java/com/galvisjuanc/play/push/web/controller/MovieController.java
+++ src/main/java/com/galvisjuanc/play/push/web/controller/MovieController.java
@@ -4,11 +4,13 @@ import com.galvisjuanc.play.push.domain.dto.MovieDto;
 import com.galvisjuanc.play.push.domain.service.MovieService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/movies")
 public class MovieController {
 
     private final MovieService movieService;
@@ -17,12 +19,12 @@ public class MovieController {
         this.movieService = movieService;
     }
 
-    @GetMapping("/movies")
+    @GetMapping
     public List<MovieDto> getAll() {
         return this.movieService.getAll();
     }
 
-    @GetMapping("/movies/{id}")
+    @GetMapping("/{id}")
     public MovieDto getById(@PathVariable long id) {
         return null;
     }
