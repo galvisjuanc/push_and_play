2df8f5799f12dcf89dcd37725d59ec473660160e
Juan Galvis <Galvis>
2026-01-04 10:58:27 -0500

    getById method implemented to return a movieDto

--- src/main/java/com/galvisjuanc/play/push/persistence/MovieEntityRepository.java
+++ src/main/java/com/galvisjuanc/play/push/persistence/MovieEntityRepository.java
@@ -3,6 +3,7 @@ package com.galvisjuanc.play.push.persistence;
 import com.galvisjuanc.play.push.domain.dto.MovieDto;
 import com.galvisjuanc.play.push.domain.repository.MovieRepository;
 import com.galvisjuanc.play.push.persistence.crud.CrudMovieEntity;
+import com.galvisjuanc.play.push.persistence.entity.MovieEntity;
 import com.galvisjuanc.play.push.persistence.mapper.MovieMapper;
 import org.springframework.stereotype.Repository;
 
@@ -23,5 +24,11 @@ public class MovieEntityRepository implements MovieRepository {
     @Override
     public List<MovieDto> getAll() {
         return this.movieMapper.toDto(this.crudMovieEntity.findAll());
+    }
+
+    @Override
+    public MovieDto getById(long id) {
+        MovieEntity movieEntity = this.crudMovieEntity.findById(id).orElse(null);
+        return movieMapper.toDto(movieEntity);
     }
 }
