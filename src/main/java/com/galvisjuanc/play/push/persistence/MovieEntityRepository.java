f416225902cd950df1fdf8651c8a338f66aef656
Juan Galvis <Galvis>
2026-01-28 13:36:45 -0500

    Update getById

--- src/main/java/com/galvisjuanc/play/push/persistence/MovieEntityRepository.java
+++ src/main/java/com/galvisjuanc/play/push/persistence/MovieEntityRepository.java
@@ -3,6 +3,7 @@ package com.galvisjuanc.play.push.persistence;
 import com.galvisjuanc.play.push.domain.dto.MovieDto;
 import com.galvisjuanc.play.push.domain.dto.UpdateMovieDto;
 import com.galvisjuanc.play.push.domain.exception.MovieAlreadyExistsException;
+import com.galvisjuanc.play.push.domain.exception.MovieNotFoundException;
 import com.galvisjuanc.play.push.domain.repository.MovieRepository;
 import com.galvisjuanc.play.push.persistence.crud.CrudMovieEntity;
 import com.galvisjuanc.play.push.persistence.entity.MovieEntity;
@@ -31,7 +32,9 @@ public class MovieEntityRepository implements MovieRepository {
 
     @Override
     public MovieDto getById(long id) {
-        return this.crudMovieEntity.findById(id).map(movieMapper::toDto).orElse(null);
+
+        MovieEntity movieEntity = this.crudMovieEntity.findById(id).orElseThrow(MovieNotFoundException::new);
+        return this.movieMapper.toDto(movieEntity);
     }
 
     @Override
