b5ae0dbbf8d352e60532821498c719723133f86f
Juan Galvis <Galvis>
2026-02-03 10:03:36 -0500

    Create a new ExceptionHandler in the RestExceptionHandler

--- src/main/java/com/galvisjuanc/play/push/web/exception/RestExceptionHandler.java
+++ src/main/java/com/galvisjuanc/play/push/web/exception/RestExceptionHandler.java
@@ -4,8 +4,12 @@ import com.galvisjuanc.play.push.domain.exception.MovieAlreadyExistsException;
 import com.galvisjuanc.play.push.domain.exception.MovieNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class RestExceptionHandler {
@@ -20,5 +24,12 @@ public class RestExceptionHandler {
     @ExceptionHandler(MovieNotFoundException.class)
     public ResponseEntity<Error> handleException(MovieNotFoundException exception) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new Error("movie-not-found", exception.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<Error>> handleException(MethodArgumentNotValidException ex) {
+        List<Error> errors = new ArrayList<>();
+
+        return ResponseEntity.badRequest().body(errors);
     }
 }
