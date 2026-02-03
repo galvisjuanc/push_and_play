f1fa324d79f5ea13412972a7e35c927c84d3eae2
Juan Galvis <Galvis>
2026-02-03 10:00:47 -0500

    Min Max annotation with Double rating

--- src/main/java/com/galvisjuanc/play/push/domain/dto/UpdateMovieDto.java
+++ src/main/java/com/galvisjuanc/play/push/domain/dto/UpdateMovieDto.java
@@ -1,5 +1,7 @@
 package com.galvisjuanc.play.push.domain.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PastOrPresent;
 
@@ -11,6 +13,9 @@ public record UpdateMovieDto(
 
         @PastOrPresent(message = "Release date should not be after the present date")
         LocalDate releaseDate,
+
+        @Min(value = 0, message = "Rating must not be less than zero.")
+        @Max(value = 5, message = "Rating must not be more than five.")
         Double rating
 ) {
 }
