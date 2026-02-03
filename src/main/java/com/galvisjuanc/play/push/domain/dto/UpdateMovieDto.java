b244753ecc9f5107ff19873d78d85cc658aaa39c
Juan Galvis <Galvis>
2026-02-03 09:55:12 -0500

    Use NotbLank in the record class

--- src/main/java/com/galvisjuanc/play/push/domain/dto/UpdateMovieDto.java
+++ src/main/java/com/galvisjuanc/play/push/domain/dto/UpdateMovieDto.java
@@ -1,8 +1,11 @@
 package com.galvisjuanc.play.push.domain.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.time.LocalDate;
 
 public record UpdateMovieDto(
+        @NotBlank(message = "Title is required")
         String title,
         LocalDate releaseDate,
         Double rating
