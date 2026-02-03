3f81cef7eaf39beb654808941d90f62211a135d0
Juan Galvis <Galvis>
2026-02-03 09:58:49 -0500

    PastOrPresent annotation used in the record class

--- src/main/java/com/galvisjuanc/play/push/domain/dto/UpdateMovieDto.java
+++ src/main/java/com/galvisjuanc/play/push/domain/dto/UpdateMovieDto.java
@@ -1,12 +1,15 @@
 package com.galvisjuanc.play.push.domain.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
 
 import java.time.LocalDate;
 
 public record UpdateMovieDto(
         @NotBlank(message = "Title is required")
         String title,
+
+        @PastOrPresent(message = "Release date should not be after the present date")
         LocalDate releaseDate,
         Double rating
 ) {
